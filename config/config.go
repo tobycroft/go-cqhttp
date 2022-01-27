a9c5fa8ed3046b2927e73579117af2cda82853cd
火线兔 <silverhawk@foxmail.com>
2022-01-27 10:37:53 +0800

    TuuzPC

--- config/config.go
+++ config/config.go
@@ -1,6 +1,6 @@
 package config
 
-const Remote_address = "http://api.tuuz.cc:15081"
+const Remote_address = "http://gobotq.tuuz.top:15081"
 
 //const Remote_address = "http://10.0.0.150:81"
 const Remote_port = 81
